1ea8793e91479baba08467b9dfb7563356ccdd60
cskawebguardian <cskawebguardian@gmail.com>
2017-04-03 22:15:35 +0300

    Ver 0.1.0

---
+++ scripts/remover.js
@@ -0,0 +1,93 @@
+var resultLinks = 'div.g:not([style*="display:none"]):not([style*="display: none"]) .r > ';
+	
+for (var i in blackList) {
+	var selector = 'a[href*="' + blackList[i] + '"]';
+	resultLinks += selector;
+	if (blackList[parseInt(i) + 1]) {
+		resultLinks += ', '
+	}
+}
+
+(function(resLinks) {
+	
+	
+    var remover = {
+        currentUrl: {}, 
+        constants: {
+            queries: {
+                result_links: resLinks, 
+                link_parent_node: '#rso div.g', 
+                main_google_node: 'main'
+            }, 
+            events: {
+                get_info: 'get_tId_and_wId', 
+                inactive: 'inactive', 
+                active: 'active'
+            }, 
+            console: {
+                needs_to_be_updated: 'selectors need to be updated!', 
+                removed: 'Malicious links were removed from this search.'
+            }, 
+            observerConfig: { childList: true, subtree: true }
+        }, 
+        init: function() {
+            var mainGoogleNode = document.getElementById(this.constants.queries.main_google_node);
+            /* avoiding google new tab page and other variations */
+            if(!mainGoogleNode) {
+                return chrome.runtime.sendMessage({ event: this.constants.events.inactive, url: window.location.href });
+            } 
+            chrome.runtime.sendMessage({ event: this.constants.events.get_info }, function(info) {
+                var tId = info.tId;
+                var wId = info.wId;
+                this.currentUrl[wId] = this.currentUrl[wId] ? this.currentUrl[wId] : {};
+                this.currentUrl[wId][tId] = window.location.href;
+                this.remove(info);
+                this.createResultsObserver(mainGoogleNode);
+            }.bind(this))
+        }, 
+        getAllW3Links: function() {
+            return document.querySelectorAll(this.constants.queries.result_links);
+        }, 
+        remove: function(info) {
+            var tId = info.tId;
+            var wId = info.wId;
+            var links = this.getAllW3Links();
+            var count = links.length;
+            if(!count) {
+                if(!this.isSameUrl(window.location.href, info)) {
+                    chrome.runtime.sendMessage({ event: this.constants.events.inactive });
+                    this.currentUrl[wId][tId] = window.location.href;
+                }
+                return;
+            }
+            this.currentUrl[wId][tId] = window.location.href;
+            chrome.runtime.sendMessage({ event: this.constants.events.active, count: count });
+            console.info(count + ' ' + this.constants.console.removed);
+            links.forEach(this.deleteOldGrandpaNode.bind(this));
+        }, 
+        createResultsObserver: function(mainGoogleNode) {
+            this.resultsObserver = new MutationObserver(function() {
+                chrome.runtime.sendMessage({ event: this.constants.events.get_info }, function(info) {
+                    var tId = info.tId;
+                    var wId = info.wId;
+                    this.currentUrl[wId] = this.currentUrl[wId] ? this.currentUrl[wId] : {};
+                    this.remove(info);
+                }.bind(this));
+            }.bind(this))
+            this.resultsObserver.observe(mainGoogleNode, this.constants.observerConfig);
+        }, 
+        isSameUrl: function(currentUrl, info) {
+            var tId = info.tId;
+            var wId = info.wId;
+            return this.currentUrl[wId][tId] === currentUrl;
+        }, 
+        deleteOldGrandpaNode: function(el) {
+            var parent = el.closest(this.constants.queries.link_parent_node);
+            if(!parent) return console.warn(this.constants.console.needs_to_be_updated);
+            parent.style.display = 'none';
+        }
+    };
+
+    remover.init();
+
+})(resultLinks);
---
+++ scripts/shared.js
@@ -0,0 +1,6 @@
+var blackList = [
+	'www.sportal.bg',
+	'cska1948.bg',
+	'prosport.bg'
+];
+
