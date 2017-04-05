7856cb10268024dcfbeb27e1c0aed1b8d811c708
cskawebguardian <cskawebguardian@gmail.com>
2017-04-05 10:11:22 +0300

    Ver 0.1.3

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,6 +1,11 @@
 var blackList = [
+	'sportal.bg',
 	'www.sportal.bg',
 	'cska1948.bg',
-	'prosport.bg'
+	'www.cska1948.bg',
+	'prosport.bg',
+	'www.prosport.bg',
+	'redpride.eu',
+	'www.redpride.eu'
 ];
 
